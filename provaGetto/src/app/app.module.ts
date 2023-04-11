d38350ce953b15ec923f1c015bf00ae360efc7cd
MarineRack <69631305+MarineRack@users.noreply.github.com>
2023-04-12 01:57:11 +0200

    head foot

--- provaGetto/src/app/app.module.ts
+++ provaGetto/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './view/sharedView/home/home.component';
+import { AltroComponent } from './view/sharedView/altro/altro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AltroComponent
   ],
   imports: [
     BrowserModule,
