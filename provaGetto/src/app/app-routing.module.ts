b7fdada963cc7df9bf2197b4551ef8d72a192bee
MarineRack <69631305+MarineRack@users.noreply.github.com>
2023-04-13 18:41:50 +0000

    folders and more

--- provaGetto/src/app/app-routing.module.ts
+++ provaGetto/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './view/sharedView/layout/layout.component';
-import { LayoutRoutingModule } from './view/sharedView/layout/layout-routing.routing';
+
 
 const routes: Routes = [
   {
@@ -20,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), LayoutRoutingModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
