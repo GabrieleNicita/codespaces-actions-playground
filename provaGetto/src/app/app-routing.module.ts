ef8823906dbd7ecbfb342579b218eaa77fac0293
MarineRack <69631305+MarineRack@users.noreply.github.com>
2023-04-16 02:29:51 +0200

    fixino

--- provaGetto/src/app/app-routing.module.ts
+++ provaGetto/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { LayoutComponent } from './view/sharedView/layout/layout.component';
-
 
 const routes: Routes = [
 
